5c24cb3e7ae993bbf4875b88ba1a661cf2ebba48
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2022-01-02 11:16:42 +0100

    *

--- ValueTypeLibrary/Core/IValueObject.cs
+++ ValueTypeLibrary/Core/IValueObject.cs
@@ -1,4 +1,20 @@
-﻿namespace ValueTypeLibrary.Core
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="IValueObject.cs" company="Lifeprojects.de">
+//     Class: IValueObject
+//     Copyright © Lifeprojects.de 2022
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>gerhard.ahrens@lifeprojects.de</email>
+// <date>02.01.2022</date>
+//
+// <summary>
+// Interface Klasse für ValueObject Basisklasse
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace ValueTypeLibrary.Core
 {
     using System;
     using System.Collections.Generic;
